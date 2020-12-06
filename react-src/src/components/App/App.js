86e4e8d59eba9941b0f3ac8ee7d147894a48a36d
Praveen <pro.appy17@gmail.com>
2020-12-05 20:04:42 -0500

    phishing

--- react-src/src/components/App/App.js
+++ react-src/src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, Button, Form, Select } from 'semantic-ui-react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,6 +12,16 @@ import UpdateRates from '../UpdateRates/UpdateRates';
 
 import logo from '../../logo.jpeg';
 import './App.css';
+const unitOptions = [
+  { key: 'Kg', text: 'Kg', value: 'Kg' },
+  { key: 'Piece', text: 'Piece', value: 'Piece' },
+  { key: 'Bunch', text: 'Bunch', value: 'Bunch' }
+]
+
+const availabilityOptions = [
+  { key: 'Available', text: 'Available', value: 'Available' },
+  { key: 'Notavailable', text: 'Not Available', value: 'Notavailable' },
+]
 
 // import {Route} from 'react-router-dom": "^5.1.2';
 
@@ -21,40 +31,92 @@ class App extends Component {
 
   constructor() {
     super();
-
+    this.state = {
+        name: '',
+        unit: '',
+        marketRate: '',
+        zfRate: '',
+        available: '',
+      }
+  
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleAvailabilityChange = this.handleAvailabilityChange.bind(this);
+    this.handleUnitChange = this.handleUnitChange.bind(this);
   }
+  handleInputChange(e) {
+        const target = e.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+    
+        this.setState({ [name]: value });
+      }
+    
+      handleUnitChange(e, data) {
+        this.setState({ unit: data.value });
+      }
+    
+      handleAvailabilityChange(e, data) {
+        this.setState({ available: data.value });
+      }
+    
+      handleSubmit(e) {
+        // Prevent browser refresh
+        e.preventDefault();
+      }
   render() {
 
     return (
       <Container>
-        <div className='App'>
-          <div className='App-header'>
-            <img src={logo} className='App-logo' alt='logo' />
-            <h1 className='App-intro'>Earthworm Operations</h1>
-          </div>
-        </div>
-        <Router>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/orders">Orders</Link>
-                </li>
-                <li>
-                  <Link to="/rates">View/Update Rates</Link>
-                </li>
-              </ul>
-            </nav>
-            <Switch>
-              <Route path="/orders">
-                <Orders />
-              </Route>
-              <Route path="/rates">
-                <UpdateRates />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
+      <Form  onSubmit={this.handleSubmit}>
+        <Form.Input
+          label='First Name'
+          type='text'
+          name='name'
+          maxLength='40'
+          required
+          value={this.state.name}
+          onChange={this.handleInputChange}
+        />
+        <Form.Input
+          label='Last Name'
+          type='text'
+          name='name'
+          maxLength='40'
+          required
+          value={this.state.name}
+          onChange={this.handleInputChange}
+        />
+        <Form.Input
+          label='Address'
+          type='text'
+          name='marketRate'
+          maxLength='40'
+          required
+          value={this.state.marketRate}
+          onChange={this.handleInputChange}
+        />
+         <Form.Input
+          label='Phone number'
+          type='text'
+          name='marketRate'
+          maxLength='40'
+          required
+          value={this.state.marketRate}
+          onChange={this.handleInputChange}
+        />
+        <Form.Input
+          label='SSN'
+          type='text'
+          name='zfRate'
+          maxLength='40'
+          required
+          value={this.state.zfRate}
+          onChange={this.handleInputChange}
+        />
+        <Button color='blue' floated='right'>Submit</Button>
+        <br /><br /> {/* Yikes! Deal with Semantic UI React! */}
+      </Form>
       </Container>
     );
   }
--- react-src/src/components/FormItem/FormItem.js
+++ react-src/src/components/FormItem/FormItem.js
@@ -1,203 +1,203 @@
-import React, { Component } from 'react';
-import { Message, Button, Form, Select } from 'semantic-ui-react';
-import axios from 'axios';
+// import React, { Component } from 'react';
 
-const unitOptions = [
-  { key: 'Kg', text: 'Kg', value: 'Kg' },
-  { key: 'Piece', text: 'Piece', value: 'Piece' },
-  { key: 'Bunch', text: 'Bunch', value: 'Bunch' }
-]
+// import axios from 'axios';
 
-const availabilityOptions = [
-  { key: 'Available', text: 'Available', value: 'Available' },
-  { key: 'Notavailable', text: 'Not Available', value: 'Notavailable' },
-]
+// const unitOptions = [
+//   { key: 'Kg', text: 'Kg', value: 'Kg' },
+//   { key: 'Piece', text: 'Piece', value: 'Piece' },
+//   { key: 'Bunch', text: 'Bunch', value: 'Bunch' }
+// ]
 
-class FormItem extends Component {
+// const availabilityOptions = [
+//   { key: 'Available', text: 'Available', value: 'Available' },
+//   { key: 'Notavailable', text: 'Not Available', value: 'Notavailable' },
+// ]
 
-  constructor(props) {
-    super(props);
+// class FormItem extends Component {
+
+//   constructor(props) {
+//     super(props);
     
-    this.state = {
-      name: '',
-      unit: '',
-      marketRate: '',
-      zfRate: '',
-      available: '',
-    }
+//     this.state = {
+//       name: '',
+//       unit: '',
+//       marketRate: '',
+//       zfRate: '',
+//       available: '',
+//     }
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleAvailabilityChange = this.handleAvailabilityChange.bind(this);
-    this.handleUnitChange = this.handleUnitChange.bind(this);
-  }
+//     this.handleInputChange = this.handleInputChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.handleAvailabilityChange = this.handleAvailabilityChange.bind(this);
+//     this.handleUnitChange = this.handleUnitChange.bind(this);
+//   }
 
-  componentWillMount() {
-    // Fill in the form with the appropriate data if user id is provided
-    if (this.props.itemID) {
-      axios.get(`${this.props.server}/api/items/${this.props.itemID}`)
-      .then((response) => {
-        this.setState({
-          name: response.data.name,
-          unit: response.data.unit,
-          marketRate: response.data.marketRate,
-          zfRate: response.data.zfRate,
-          available: response.data.available,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    }
-  }
+//   componentWillMount() {
+//     // Fill in the form with the appropriate data if user id is provided
+//     if (this.props.itemID) {
+//       axios.get(`${this.props.server}/api/items/${this.props.itemID}`)
+//       .then((response) => {
+//         this.setState({
+//           name: response.data.name,
+//           unit: response.data.unit,
+//           marketRate: response.data.marketRate,
+//           zfRate: response.data.zfRate,
+//           available: response.data.available,
+//         });
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//     }
+//   }
 
-  handleInputChange(e) {
-    const target = e.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
+//   handleInputChange(e) {
+//     const target = e.target;
+//     const value = target.type === 'checkbox' ? target.checked : target.value;
+//     const name = target.name;
 
-    this.setState({ [name]: value });
-  }
+//     this.setState({ [name]: value });
+//   }
 
-  handleUnitChange(e, data) {
-    this.setState({ unit: data.value });
-  }
+//   handleUnitChange(e, data) {
+//     this.setState({ unit: data.value });
+//   }
 
-  handleAvailabilityChange(e, data) {
-    this.setState({ available: data.value });
-  }
+//   handleAvailabilityChange(e, data) {
+//     this.setState({ available: data.value });
+//   }
 
-  handleSubmit(e) {
-    // Prevent browser refresh
-    e.preventDefault();
+//   handleSubmit(e) {
+//     // Prevent browser refresh
+//     e.preventDefault();
 
-    const user = {
-      name: this.state.name,
-      unit: this.state.unit,
-      marketRate: this.state.marketRate,
-      zfRate: this.state.zfRate,
-      available: this.state.available
-    }
-    // Acknowledge that if the user id is provided, we're updating via PUT
-    // Otherwise, we're creating a new data via POST
-    const method = this.props.itemID ? 'put' : 'post';
-    const params = this.props.itemID ? this.props.itemID : '';
+//     const user = {
+//       name: this.state.name,
+//       unit: this.state.unit,
+//       marketRate: this.state.marketRate,
+//       zfRate: this.state.zfRate,
+//       available: this.state.available
+//     }
+//     // Acknowledge that if the user id is provided, we're updating via PUT
+//     // Otherwise, we're creating a new data via POST
+//     const method = this.props.itemID ? 'put' : 'post';
+//     const params = this.props.itemID ? this.props.itemID : '';
 
-    axios({
-      method: method,
-      responseType: 'json',
-      url: `${this.props.server}/api/items/${params}`,
-      data: user
-    })
-    .then((response) => {
-      this.setState({
-        formClassName: 'success',
-        formSuccessMessage: response.data.msg
-      });
+//     axios({
+//       method: method,
+//       responseType: 'json',
+//       url: `${this.props.server}/api/items/${params}`,
+//       data: user
+//     })
+//     .then((response) => {
+//       this.setState({
+//         formClassName: 'success',
+//         formSuccessMessage: response.data.msg
+//       });
 
-      if (!this.props.itemID) {
-        this.setState({
-          name: '',
-          unit: '',
-          marketRate: '',
-          zfRate: '',
-          available: ''
-        });
-        this.props.onItemAdded(response.data.result);
-      }
-      else {
-        this.props.onItemUpdated(response.data.result);
-      }
+//       if (!this.props.itemID) {
+//         this.setState({
+//           name: '',
+//           unit: '',
+//           marketRate: '',
+//           zfRate: '',
+//           available: ''
+//         });
+//         this.props.onItemAdded(response.data.result);
+//       }
+//       else {
+//         this.props.onItemUpdated(response.data.result);
+//       }
       
-    })
-    .catch((err) => {
-      if (err.response) {
-        if (err.response.data) {
-          this.setState({
-            formClassName: 'warning',
-            formErrorMessage: err.response.data.msg
-          });
-        }
-      }
-      else {
-        this.setState({
-          formClassName: 'warning',
-          formErrorMessage: 'Something went wrong. ' + err
-        });
-      }
-    });
-  }
+//     })
+//     .catch((err) => {
+//       if (err.response) {
+//         if (err.response.data) {
+//           this.setState({
+//             formClassName: 'warning',
+//             formErrorMessage: err.response.data.msg
+//           });
+//         }
+//       }
+//       else {
+//         this.setState({
+//           formClassName: 'warning',
+//           formErrorMessage: 'Something went wrong. ' + err
+//         });
+//       }
+//     });
+//   }
 
-  render() {
+//   render() {
 
-    const formClassName = this.state.formClassName;
-    const formSuccessMessage = this.state.formSuccessMessage;
-    const formErrorMessage = this.state.formErrorMessage;
+//     const formClassName = this.state.formClassName;
+//     const formSuccessMessage = this.state.formSuccessMessage;
+//     const formErrorMessage = this.state.formErrorMessage;
 
-    return (
-      <Form className={formClassName} onSubmit={this.handleSubmit}>
-        <Form.Input
-          label='Name'
-          type='text'
-          name='name'
-          maxLength='40'
-          required
-          value={this.state.name}
-          onChange={this.handleInputChange}
-        />
-        <Form.Field
-          control={Select}
-          label='Unit'
-          options={unitOptions}
-          placeholder='Unit'
-          value={this.state.unit}
-          selected= {this.state.unit}
-          onChange={this.handleUnitChange}
-        />
-        <Form.Input
-          label='Market Rate'
-          type='text'
-          name='marketRate'
-          maxLength='40'
-          required
-          value={this.state.marketRate}
-          onChange={this.handleInputChange}
-        />
-        <Form.Input
-          label='ZoomFresh Rate'
-          type='text'
-          name='zfRate'
-          maxLength='40'
-          required
-          value={this.state.zfRate}
-          onChange={this.handleInputChange}
-        />
-        <Form.Field
-          control={Select}
-          label='Availability'
-          options={availabilityOptions}
-          placeholder='Availability'
-          value={this.state.available}
-          selected= {this.state.available}
-          onChange={this.handleAvailabilityChange}
-        />
-        <Message
-          success
-          color='green'
-          header='Nice one!'
-          content={formSuccessMessage}
-        />
-        <Message
-          warning
-          color='yellow'
-          header='Woah!'
-          content={formErrorMessage}
-        />
-        <Button color={this.props.buttonColor} floated='right'>{this.props.buttonSubmitTitle}</Button>
-        <br /><br /> {/* Yikes! Deal with Semantic UI React! */}
-      </Form>
-    );
-  }
-}
+//     return (
+//       <Form className={formClassName} onSubmit={this.handleSubmit}>
+//         <Form.Input
+//           label='Name'
+//           type='text'
+//           name='name'
+//           maxLength='40'
+//           required
+//           value={this.state.name}
+//           onChange={this.handleInputChange}
+//         />
+//         <Form.Field
+//           control={Select}
+//           label='Unit'
+//           options={unitOptions}
+//           placeholder='Unit'
+//           value={this.state.unit}
+//           selected= {this.state.unit}
+//           onChange={this.handleUnitChange}
+//         />
+//         <Form.Input
+//           label='Market Rate'
+//           type='text'
+//           name='marketRate'
+//           maxLength='40'
+//           required
+//           value={this.state.marketRate}
+//           onChange={this.handleInputChange}
+//         />
+//         <Form.Input
+//           label='ZoomFresh Rate'
+//           type='text'
+//           name='zfRate'
+//           maxLength='40'
+//           required
+//           value={this.state.zfRate}
+//           onChange={this.handleInputChange}
+//         />
+//         <Form.Field
+//           control={Select}
+//           label='Availability'
+//           options={availabilityOptions}
+//           placeholder='Availability'
+//           value={this.state.available}
+//           selected= {this.state.available}
+//           onChange={this.handleAvailabilityChange}
+//         />
+//         <Message
+//           success
+//           color='green'
+//           header='Nice one!'
+//           content={formSuccessMessage}
+//         />
+//         <Message
+//           warning
+//           color='yellow'
+//           header='Woah!'
+//           content={formErrorMessage}
+//         />
+//         <Button color={this.props.buttonColor} floated='right'>{this.props.buttonSubmitTitle}</Button>
+//         <br /><br /> {/* Yikes! Deal with Semantic UI React! */}
+//       </Form>
+//     );
+//   }
+// }
 
-export default FormItem;
+// export default FormItem;
