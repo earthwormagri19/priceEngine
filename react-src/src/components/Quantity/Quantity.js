7697a81d97128bf5c5069a4daae97d5ad651194d
aparnagireddy <52471132+aparnagireddy@users.noreply.github.com>
2021-05-19 22:33:05 -0400

    added mangoes

--- react-src/src/components/Quantity/Quantity.js
+++ react-src/src/components/Quantity/Quantity.js
@@ -284,6 +284,15 @@ class Quantity extends Component {
                 "OrderList": []
             },
             {
+                "productName": "Banganapalli Mangoes Box -2Kg",
+                "qty": 0,
+                "baseQty": 2,
+                "totalQty": 0,
+                "unit": "Kg",
+                "packingList": [],
+                "OrderList": []
+            },
+            {
                 "productName": "Pomegranate",
                 "qty": 0,
                 "baseQty": 0.5,
